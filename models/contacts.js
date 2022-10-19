41dfed0263e6e16f94a51d04462085d0e8967511
Paresh B. Patel <pareshbkpatel@gmail.com>
2022-10-19 13:12:54 +0100

    Contacts Schema Completed

--- models/contacts.js
+++ models/contacts.js
@@ -1,11 +1,35 @@
 const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
-  snaNumber: {
+  id: {
+    type: Number,
+    required: true
+  },
+  first_name: {
     type: String,
     required: true
   },
-  snaName: {
+  last_name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  date_of_birth: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  phone: {
     type: String,
     required: true
   }
